42e8acfa9473d5eab81b3c8df47f837a7ac31031
Bard-Gaming <christophe@dronne.fr>
2025-12-30 14:25:48 +0100

    feat: add header for common utilites

---
+++ include/project_starter/common.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2025
+** Module: Common
+** File description:
+** Common utilities for the
+** project-start project
+*/
+
+#ifndef PROJECT_STARTER_COMMON_H
+    #define PROJECT_STARTER_COMMON_H
+
+    #ifndef PROJECT_START_CONFIG_PATH
+        #define PROJECT_START_CONFIG_PATH "~/.config/project-start/"
+    #endif
+
+#endif
