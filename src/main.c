895c00f401af5183815eb6d5b0ba99d384decc88
Bard-Gaming <christophe@dronne.fr>
2025-12-31 01:33:12 +0100

    feat: add more options to cli

--- src/main.c
+++ src/main.c
@@ -34,7 +34,9 @@ static const char doc[] =
 static const char args_doc[] = "NAME [DISPLAY_NAME]";
 
 static const struct argp_option options[] = {
-    { "language", 'l', "LANGUAGE", 0, "Choose the project's programming language" },
+    { "language", 'l', "LANGUAGE", 0, "Choose the project's programming language (default: C)" },
+    { "year", 'y', "YEAR", 0, "Set the 'year' variable for templates (default: current year)" },
+    { "organisation", 'o', "ORGANISATION", 0, "Set the 'organisation' variable for templates (default: EPITECH)" },
     { 0 }
 };
 
@@ -43,8 +45,14 @@ static error_t parse_option(int key, char *arg, struct argp_state *state)
     TemplateContext *context = state->input;
 
     switch (key) {
+        case 'o':
+            hashtable_set(&context->variables, "organisation", arg);
+            return 0;
         case 'l':
             context->language = arg;
+            return 0;
+        case 'y':
+            hashtable_set(&context->variables, "year", arg);
             return 0;
 
         case ARGP_KEY_ARG:
@@ -80,4 +88,6 @@ int main(int argc, char *argv[])
     template_register_context_names(&context);
 
     template_generate(&context);
+
+    hashtable_delete(&context.variables, NULL);
 }
