a03417639646ce9cc2a4a612cef53514ef7e6a7b
Bard-Gaming <christophe@dronne.fr>
2026-01-02 15:39:20 +0100

    feat(vector): add header file

---
+++ include/project_starter/vector.h
@@ -0,0 +1,24 @@
+/*
+** BARDSHELL PROJECT, 2025
+** Module: Vector
+** File description:
+** Header file for everything
+** related to vectors
+*/
+
+#ifndef PROJECT_STARTER_VECTOR_H
+    #define PROJECT_STARTER_VECTOR_H
+    #include <stddef.h>
+
+
+typedef struct {
+    void *data;
+    size_t count;
+    size_t capacity;
+} Vector;
+
+
+void vector_push_back(Vector *vector, void *data);
+
+
+#endif
