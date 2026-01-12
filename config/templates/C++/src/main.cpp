65f5d1d3dd7e84e5cd94025365218df651faac9b
Bard-Gaming <christophe@dronne.fr>
2026-01-12 09:37:27 +0100

    feat(config): add C++ main file

---
+++ config/templates/C++/src/main.cpp
@@ -0,0 +1,17 @@
+/*
+** {{ORGANISATION}} PROJECT, {{year}}
+** {{display_name}}
+** File description:
+** Program Entry
+*/
+
+#include <iostream>
+
+
+/**
+ * Program Entry
+ */
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello, World!" << std::endl;
+}
